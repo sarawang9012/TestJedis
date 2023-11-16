5b25c35b95e98d14bd91cd2ef04b914f6b0d9f68
sarawang9012 <625504790@qq.com>
2023-11-16 18:07:23 +0800

    Initial commit

---
+++ src/main/java/org/how2java/Main.java
@@ -0,0 +1,12 @@
+package org.how2java;/**
+* ClassName: ${NAME}
+* Package: org.how2java
+* Description:
+* @Author Sara Wang
+* @Create ${DATE} ${TIME}
+* @Version 1.0
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/redis/TestRedisManyCommands.java
@@ -0,0 +1,9 @@
+package redis;/**
+* ClassName: TestRedisManyCommands
+* Package: redis
+* Description:
+* @Author Sara Wang
+* @Create 2023/11/16 17:14
+* @Version 1.0
+*/public class TestRedisManyCommands {
+}
